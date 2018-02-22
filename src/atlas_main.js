bf4fa78f4b46b5474215681320f605c27c3abb59
Mark Kurt <markkurt@fastmail.fm>
2018-02-21 19:36:54 -0600

    include adapter in exports

--- src/atlas_main.js
+++ src/atlas_main.js
@@ -2,4 +2,4 @@ var AtlasSpriteSheetPlayer = require('./atlas_sprite_sheet_player.js').AtlasSpri
 var AtlasAPIAdapter = require('./atlas_api_adapter.js').AtlasAPIAdapter;
 var AtlasSPAdapter = require('./atlas_sp_adapter.js').AtlasSPAdapter;
 
-export { AtlasSpriteSheetPlayer, AtlasAPIAdapter };
+export { AtlasSpriteSheetPlayer, AtlasAPIAdapter, AtlasSPAdapter };
